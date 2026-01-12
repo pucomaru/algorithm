0c4124b4996fd0a1cb0381d51aff4e5220410a9e
다예 <myannnng@gmail.com>
2026-01-12 18:58:17 +0900

    Solving

--- src/baekjoon/p12865/Main.java
+++ src/baekjoon/p12865/Main.java
@@ -18,7 +18,7 @@ public class Main {
     // DP
     static int[][] dp ;
 
-    public static void main(String args[]) throws IOException {
+    public static void 평범한 배낭(String args[]) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
 
