2224f68aadaf977f4e6247e1950a251a63aca804
다예 <myannnng@gmail.com>
2026-01-30 21:24:08 +0900

    Solving

---
+++ src/baekjoon/p1012/유기농배추.java
@@ -0,0 +1,25 @@
+package baekjoon.p1012;
+
+import java.io.*;
+import java.util.*;
+
+public class 유기농배추 {
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int tc;
+    static int M,N,K;
+
+    public static void main(String[] args) throws IOException{
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 테스트 케이스
+        tc = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < tc; i++){
+
+        }
+
+    }
+}
